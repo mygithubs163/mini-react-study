bca435d2f3d9e61b9598c4354ec4aa5f15443fdd
mygithubs163 <1323959330@qq.com>
2024-09-10 09:14:52 +0800

    feat: mini-react page

---
+++ V02-1/App.jsx
@@ -0,0 +1,5 @@
+import React from './vite-project/core/React.js'
+
+const App = React.createElement('div',{id:'app'},'hi-', 'mini-react');
+
+export default App;
---
+++ V02-1/index.jsx
@@ -0,0 +1,4 @@
+import ReactDOM from "./vite-project/core/ReactDom.js";
+import App from "./App.jsx";
+
+ReactDOM.creatRoot(document.querySelector('#root')).render(App);
